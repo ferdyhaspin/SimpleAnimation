533970ffee25a996bb2d6846c555fd79aa2912d9
ferdyhaspin <ferdihaspin@gmail.com>
2020-03-30 19:42:24 +0700

    Initial Commit

---
+++ app/src/main/java/com/ferdyhaspin/simpleanimation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ferdyhaspin.simpleanimation
+
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 0.5f, 1f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.5f, 1f)
+        val alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 0f, 1f)
+        btnStart.setOnClickListener {
+            ObjectAnimator.ofPropertyValuesHolder(ivLogo, scaleX, scaleY, alpha).apply {
+                interpolator = OvershootInterpolator()
+                duration = 1000
+            }.start()
+        }
+    }
+}
